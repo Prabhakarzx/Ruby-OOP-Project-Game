c6de1bb56220ba432adf18dfdcd081597070d7a5
prabhakarzx <ppandey2098@gmail.com>
2020-08-14 03:57:40 +0530

    Add Game States for players

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,19 @@
 class Player
   attr_reader :name, :symbol, :array
+  attr_accessor :score
 
   def initialize(name, symbol)
     @array = []
     @name = name
     @symbol = symbol
+    @score = 0
   end
 
   def array=(number)
     array.push(number)
   end
-  
-  def clear_array 
-    self.array.clear
+
+  def clear_array
+    array.clear
   end
 end
