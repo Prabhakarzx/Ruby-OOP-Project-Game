09bf9319c514071269b08620dfad88ead049dc89
prabhakarzx <ppandey2098@gmail.com>
2020-08-13 21:35:45 +0530

    add update move function

--- lib/board.rb
+++ lib/board.rb
@@ -41,9 +41,28 @@ class Logic
   attr_reader :board
   WINNING_MOVES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
   def initialize
-    @player1_array = Players.player1_array
-    @player2_array = Players.player2_array
+    @player1_array = Player.player1_array
+    @player2_array = Player.player2_array
   end
 
+  
+
+
+
+
+  def update(move, current_player)
+    return false if @board[move - 1] == 'X' || @board[move - 1] == 'O'
+
+    @board[move - 1] = current_player[0]
+    if current_player[0] == 'X'
+      @player1_array.push(move)
+      check_move(@player1_array)
+    elsif current_player[0] == 'O'
+      @player2_array.push(move)
+      check_move(@player2_array)
+    end
+  end
+
+ 
   
 end
