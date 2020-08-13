c19ae622419429b91ea4fa93076d2b6059ef8f3d
prabhakarzx <ppandey2098@gmail.com>
2020-08-13 21:42:42 +0530

    add player logic

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,21 @@
 class Player
   attr_reader :name, :symbol
+  attr_accessor :player1, :player2, :player1_array, :player2_array
+
   def initialize(name, symbol)
     @players = {}
     @name = name
     @symbol = symbol
     @players[@name] = @symbol
   end
+
+  def self.player1_array
+    @player1_array = []
+  end
+
+  def self.player2_array
+    @player2_array = []
+  end
+
 end
+
