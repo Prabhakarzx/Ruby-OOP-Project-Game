a1ad2d4e982c3754dbfee9dfd9efb13059b176b8
victorG <peter200970@hotmail.com>
2020-08-13 09:51:21 -0500

    array now 1 dimensional

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,33 @@
 class Board
   def create_board
-    a = %w[1 2 3]
-    b = %w[4 5 6]
-    c = %w[7 8 9]
-    @board = [a, b, c]
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def display_board
-    p @board[0]
-    p @board[1]
-    p @board[2]
+    p @board[0..2]
+    p @board[3..5]
+    p @board[6..9]
   end
 
   def place_symbol(position, symbol)
     @position = position
     @symbol = symbol
-    if @position < 4
-      replace(0)
-    elsif @position > 3 && @position < 7
-      replace(1)
-    elsif @position > 6
-      replace(2)
-    else
-      puts 'does not exist you lost your turn'
-
-    end
+      @board.each_with_index do |element, index|
+        if element == @position 
+          @board[index] = @symbol
+        end
+      end
   end
 
-  def replace(index)
-    @position = @position.to_s
-    @board[index].each_with_index do |element, index1|
-      @board[index][index1] = @symbol if element == @position
+  def validate (turn)
+    validate = false
+    
+    @board.each_with_index do |element|
+      if element == turn 
+        validate = true
+        break
+      end
     end
-  end
+    
+  end 
 end
