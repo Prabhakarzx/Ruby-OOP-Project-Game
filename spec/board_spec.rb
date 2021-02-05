56b42708a9fe9cae37ad90e3ce95eb507331324a
prabhakarzx <ppandey2098@gmail.com>
2021-02-05 23:44:45 +0530

    update rspec for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,11 @@ require_relative '../lib/player.rb'
 
 describe Board do
   let(:player) { Player.new('Ppandey', 'X') }
-  let(:baord) { Board.new }
+  let(:board) { Board.new }
 
   describe 'create_board' do
-    it 'create the baord' do
-      expect(baord.create_board(baord)).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    it 'create the board' do
+      expect(board.create_board(board)).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
